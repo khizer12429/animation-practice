e0ef1ecefb28acdcf8766bbf081d586f3334db91
khizer12429 <khizerfarooq267@gmail.com>
2024-10-30 18:11:10 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+gsap.to(".image1 img",{
+    width:"100%",
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2
+})
+gsap.to(".text h1",{
+    top: "0",
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2
+})
+gsap.to(".text h1",{
+    top: "-100",
+    delay: 2,
+    ease:Expo.easeInOut,
+    duration:2,
+    stagger:2
+})
